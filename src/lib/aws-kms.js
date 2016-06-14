8b7b8e27da083d8b3962a690915cfe58611e2b45
Brian Jacobel <bjacobel@gmail.com>
2016-06-14 08:48:07 -0400

    Jest mocks are fun

--- src/lib/aws-kms.js
+++ src/lib/aws-kms.js
@@ -1,6 +1,6 @@
 import { writeToFile } from './io';
 
-export const getVinzKeyArn = (kmsClient) => {
+const getVinzKeyArn = (kmsClient) => {
   return new Promise((resolve, reject) => {
     kmsClient.listAliases({}, (err, data) => {
       if (err) {
@@ -10,13 +10,18 @@ export const getVinzKeyArn = (kmsClient) => {
       }
     });
   }).then((data) => {
-    return data;
+    return data.Aliases.filter(x => x.AliasName === 'vinz')[0].AliasArn;
   });
+};
+
+const encryptData = (keyArn, secretValue) => {
+
 };
 
 export const encryptAndStore = (kmsClient, secretName, secretValue) => {
   return getVinzKeyArn(kmsClient)
     .then((keyArn) => {
+      console.log(keyArn, secretValue)
       encryptData(keyArn, secretValue);
     }).then((encryptedSecret) => {
       writeToFile(encryptedSecret, secretName);
--- tests/lib/aws-kms-test.js
+++ tests/lib/aws-kms-test.js
@@ -2,7 +2,45 @@ jest.unmock('../../src/lib/aws-kms');
 
 const kms = require('../../src/lib/aws-kms');
 
-describe('getVinzKeyArn', () => {
-});
+describe('module', () => {
+  let kmsClient;
+  beforeEach(() => {
+    kmsClient = {
+      listAliases: jest.fn((opts, callback) => {
+        callback(null, {
+          Aliases: [
+            {
+              AliasName: 'not vinz',
+              AliasArn: '1234',
+              TargetKeyId: 'abcde'
+            },
+            {
+              AliasName: 'vinz',
+              AliasArn: '5678',
+              TargetKeyId: 'fghjk'
+            }
+          ]
+        });
+      })
+    };
+  });
 
-describe
+  describe('encryptAndStore', () => {
+    // beforeEach(() => {
+    //   kms.getVinzKeyArn = jest.fn(() => new Promise(() => 'arn'));
+    //   kms.encryptData = jest.fn(() => new Promise(() => 'encrypted'));
+    // });
+
+    it('calls encryptData after getVinzKeyArn returns', () => {
+      return kms.encryptAndStore(kmsClient, 'foo', 'bar').then(() => {
+        expect(kms.encryptData).toBeCalledWith('arn', 'bar');
+      });
+    });
+
+    it('calls writeToFile with the results of encryptData after that returns', () => {
+      return kms.encryptAndStore(kmsClient, 'foo', 'bar').then(() => {
+        expect(kms.writeToFile).toBeCalledWith('encrypted', 'foo');
+      });
+    });
+  });
+});
